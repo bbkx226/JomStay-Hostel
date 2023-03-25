7a5759b18a09b7c67628edb1a6fcafd5be114090
Brandon <bbkx226@gmail.com>
2023-03-25 16:21:21 +0800

    Created Java Main Default File

---
+++ src/main/java/hostelsystem/HostelSystem.java
@@ -0,0 +1,14 @@
+package hostelsystem;
+/**
+ *
+ * @author bbkx
+ */
+
+import DesignUI.Splash;
+
+public class HostelSystem {
+
+    public static void main(String[] args) {
+        new Splash().start();
+    }
+}
