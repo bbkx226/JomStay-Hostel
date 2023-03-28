3e530c4c843d9dafeea74434a70b72db9e2b37d7
Brandon <bbkx226@gmail.com>
2023-03-29 00:21:26 +0800

    Created Application Object Handler

---
+++ src/main/java/Utils/ApplicationHandling.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utils;
+
+import Models.Application;
+import Models.Room;
+import Models.Student;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author bbkx2
+ */
+public class ApplicationHandling {
+    private static final String PATH = "src/main/java/databases/application.txt";
+    
+    private final ArrayList<Room> totalRooms = new RoomHandling().totalRooms;
+    private final ArrayList<Student> totalStudents = new StudentHandling().totalStudents;    
+    public ArrayList<Application> totalApplications = getTotalApplications();
+    public ArrayList<Application> pendingApplications = getPendingApplications();
+    
+    private ArrayList<Application> getTotalApplications(){
+        ArrayList<Application> buffer = new ArrayList<>();
+        
+        File file = new File(PATH);
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(" ");
+                Student student = compareToStudent(data[1]);
+                Room room = compareToRoom(data[2]);
+                if(student != null && room != null){
+                    Application application = new Application(data[0], student, room, data[3], data[4], data[5], data[6]);
+                    buffer.add(application);
+                }
+            }
+            reader.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return buffer;
+    }
+    
+    public static void updateApplicationFile(ArrayList<Application> applications){
+        File file = new File(PATH);
+        try(PrintWriter printWriter = new PrintWriter(new FileWriter(file, false))){
+            printWriter.flush();
+            for (Application application : applications){
+                printWriter.append(String.format("%s %s %s %s %s %s %s\n", application.getApplicationID(), application.getStudent().getID(), application.getRoom().getRoomID(), application.getStatus(), application.getCreateDate(), application.getStartDate(), application.getEndDate()));
+            }
+            printWriter.close();
+        } catch(IOException e){
+            e.printStackTrace();        
+        }
+    }
+    
+    private ArrayList<Application> getPendingApplications(){
+        ArrayList<Application> total = getTotalApplications();
+        ArrayList<Application> buffer = new ArrayList<>();
+        try{
+            for(Application application : total){
+                if(application.getStatus().equals("Pending")){
+                    buffer.add(application);
+                }
+            }
+        } catch(Exception e){
+            e.printStackTrace();  
+        }
+        return buffer;
+    }
+    
+    private Student compareToStudent(String studentID){
+        for (Student student : totalStudents){
+            if(studentID.equals(student.getID())){
+                return student;
+            }
+        }
+        return null;
+    }
+    
+    private Room compareToRoom(String roomID){
+        for (Room room : totalRooms){
+            if(roomID.equals(room.getRoomID())){
+                return room;
+            }
+        }
+        return null;
+    }
+}
