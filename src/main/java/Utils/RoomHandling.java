aa9148c5ccfef117744c3cf6f822258791316967
QuasarX35 <shootingquasar555@gmail.com>
2023-03-29 14:49:16 +0800

    updated RoomHandling.java

--- src/main/java/Utils/RoomHandling.java
+++ src/main/java/Utils/RoomHandling.java
@@ -12,54 +12,38 @@ import java.util.ArrayList;
 public class RoomHandling {
     private static final String PATH = "src/main/java/databases/room.txt";
     public ArrayList<Room> totalRooms = getRooms();
-      
-    private ArrayList<Room> getRooms(){
+    
+    private ArrayList<Room> getRooms() {
         ArrayList<Room> buffer = new ArrayList<>();
-        
-        File file = new File(PATH);
-        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] data = line.split(" ");
-                Room room = new Room(data[0], data[1], Boolean.parseBoolean(data[2]), Integer.parseInt(data[3]), Integer.parseInt(data[4]));
-                buffer.add(room);
-            }
-            reader.close();
-        } catch (IOException e){
-            e.printStackTrace();
+        for (String line : FileHandlerUtils.readLines(PATH)) {
+            String[] data = line.split(" ");
+            Room room = new Room(data[0], data[1], Boolean.parseBoolean(data[2]), Integer.parseInt(data[3]), Integer.parseInt(data[4]));
+            buffer.add(room);
         }
         return buffer;
     }
     
-    public static void updateRoomFile(ArrayList<Room> rooms){
-        File file = new File(PATH);
-        try(PrintWriter printWriter = new PrintWriter(new FileWriter(file, false))){
-            printWriter.flush();
-            for (Room room : rooms){
-                printWriter.append(String.format("%s %s %b %d %d\n", room.getRoomID(), room.getStatus(), room.isServicing(), room.getPax(), room.getPricePerPax()));
-            }
-            printWriter.close();
-        } catch(IOException e){
-            e.printStackTrace();        
+    public static void updateRoomFile(ArrayList<Room> rooms) {
+        String roomListString = "";
+        for (Room room : rooms) {
+            roomListString += room.toString();
         }
+        FileHandlerUtils.writeString(PATH, roomListString, false);
     }
     
-    public static void deleteRoomData(ArrayList<Room> rooms, String roomID){
+    public static void deleteRoomData(ArrayList<Room> rooms, String roomID) {
         int i = 1;
-        File file = new File(PATH);
-        try(PrintWriter printWriter = new PrintWriter(new FileWriter(file, false))){
-            printWriter.flush();
-            for (Room room : rooms){
-                if(room.getRoomID().equals(roomID)){ 
-                    continue;
-                }
-                printWriter.append(String.format("R%03d %s %b %d %d\n", i, room.getStatus(), room.isServicing(), room.getPax(), room.getPricePerPax()));
-                i++;
+        String roomListString = "";
+        for (Room room : rooms) {
+            if (room.getRoomID().equals(roomID)) {
+                continue;
             }
-            printWriter.close();
-        } catch(IOException e){
-            e.printStackTrace();        
+            roomListString += String.format("R%03d %s %b %d %d\n", 
+                                            i, room.getStatus(), 
+                                            room.isServicing(), room.getPax(), 
+                                            room.getPricePerPax());
         }
+        FileHandlerUtils.writeString(PATH, roomListString, false);
     }
     
     public static void appendRoomFile(ArrayList<Room> rooms){
@@ -71,5 +55,9 @@ public class RoomHandling {
         } catch(IOException e){
             e.printStackTrace();        
         }    
+    }
+    
+    public static void appendRoomFile(Room room) {
+        FileHandlerUtils.writeString(PATH, room.toString(), true);
     }
 }
