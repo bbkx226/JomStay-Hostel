0d707142715341b4e8945171402d4139fb9ec293
Brandon <bbkx226@gmail.com>
2023-03-30 00:21:24 +0800

    Created Admin Data Function

--- src/main/java/Utils/UserHandling.java
+++ src/main/java/Utils/UserHandling.java
@@ -4,21 +4,25 @@
  */
 package Utils;
 
+import Models.Admin;
 import Models.Student;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 /**
  *
  * @author bbkx2
  */
-public class StudentHandling {
+public class UserHandling {
     private static final String PATH = "src/main/java/databases/auth.txt";
     public ArrayList<Student> totalStudents = getStudents();
-      
+    public ArrayList<Admin> totalAdmins = getAdmins();  
+    
     private ArrayList<Student> getStudents(){
         ArrayList<Student> buffer = new ArrayList<>();
         
@@ -37,5 +41,42 @@ public class StudentHandling {
             e.printStackTrace();
         }
         return buffer;
-    }    
+    }
+    
+    private static ArrayList<Admin> getAdmins(){
+        ArrayList<Admin> buffer = new ArrayList<>();
+        
+        File file = new File(PATH);
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(" ");
+                if(data[0].contains("AD")){
+                    Admin admin = new Admin(data[0], data[1], data[2], data[3], data[4]);
+                    buffer.add(admin);  
+                }
+            }
+            reader.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return buffer;
+    }
+    
+    public static void updateStudentFile(ArrayList<Student> students){
+        File file = new File(PATH);
+        ArrayList<Admin> admins = getAdmins();
+        try(PrintWriter printWriter = new PrintWriter(new FileWriter(file, false))){
+            printWriter.flush();
+            for (Admin admin : admins) {
+                printWriter.append(String.format("%s %s %s %s %s \n", admin.getID(), admin.getName(),admin.getEmail(), admin.getUsername(), admin.getPassword()));
+            }
+            for (Student student : students){
+                printWriter.append(String.format("%s %s %s %s %s %s %s %s %s\n", student.getID(), student.getName(), student.getEmail(), student.getUsername(), student.getGender(), student.getLoginDate(), student.getPassword(), student.getPhoneNo(), student.getNRIC()));
+            }
+            printWriter.close();
+        } catch(IOException e){
+            e.printStackTrace();        
+        }
+    }
 }
