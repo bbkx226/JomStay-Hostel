fb1a9e6d41fc303771775707b89761ff9d64870e
QuasarX35 <shootingquasar555@gmail.com>
2023-04-02 19:45:31 +0800

    "added extra student details"

--- src/main/java/Models/Student.java
+++ src/main/java/Models/Student.java
@@ -7,7 +7,8 @@ import java.util.Objects;
  * @author bbkx2
  */
 public class Student extends User {
-    private String email, gender, loginDate, phoneNo, NRIC;
+    private String gender, loginDate, phoneNo, NRIC;
+    private static String nationality, race, religion, permanentAddress, medicalCondition, emerContactName, emerContactRelationship, emerContactNo;
 
     public Student(String ID, String name, String email, String username, String gender, String loginDate, String password, String phoneNo, String NRIC) {
         super(ID, name, email, username, password);
@@ -33,6 +34,38 @@ public class Student extends User {
         return NRIC;
     }
 
+    public static String getNationality() {
+        return nationality;
+    }
+
+    public static String getRace() {
+        return race;
+    }
+
+    public static String getReligion() {
+        return religion;
+    }
+
+    public static String getPermanentAddress() {
+        return permanentAddress;
+    }
+
+    public static String getMedicalCondition() {
+        return medicalCondition;
+    }
+
+    public static String getEmerContactName() {
+        return emerContactName;
+    }
+
+    public static String getEmerContactRelationship() {
+        return emerContactRelationship;
+    }
+
+    public static String getEmerContactNo() {
+        return emerContactNo;
+    }
+
     public void setGender(String gender) {
         this.gender = gender;
     }
@@ -49,10 +82,41 @@ public class Student extends User {
         this.NRIC = NRIC;
     }
 
+    public static void setNationality(String nationality) {
+        Student.nationality = nationality;
+    }
+
+    public static void setRace(String race) {
+        Student.race = race;
+    }
+
+    public static void setReligion(String religion) {
+        Student.religion = religion;
+    }
+
+    public static void setPermanentAddress(String permanentAddress) {
+        Student.permanentAddress = permanentAddress;
+    }
+
+    public static void setMedicalCondition(String medicalCondition) {
+        Student.medicalCondition = medicalCondition;
+    }
+
+    public static void setEmerContactName(String emerContactName) {
+        Student.emerContactName = emerContactName;
+    }
+
+    public static void setEmerContactRelationship(String emerContactRelationship) {
+        Student.emerContactRelationship = emerContactRelationship;
+    }
+
+    public static void setEmerContactNo(String emerContactHPNo) {
+        Student.emerContactNo = emerContactHPNo;
+    }
+
     @Override
     public int hashCode() {
         int hash = 7;
-        hash = 67 * hash + Objects.hashCode(this.email);
         hash = 67 * hash + Objects.hashCode(this.gender);
         hash = 67 * hash + Objects.hashCode(this.loginDate);
         hash = 67 * hash + Objects.hashCode(this.phoneNo);
@@ -72,9 +136,6 @@ public class Student extends User {
             return false;
         }
         final Student other = (Student) obj;
-        if (!Objects.equals(this.email, other.email)) {
-            return false;
-        }
         if (!Objects.equals(this.gender, other.gender)) {
             return false;
         }
@@ -85,6 +146,30 @@ public class Student extends User {
             return false;
         }
         return Objects.equals(this.NRIC, other.NRIC);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Student{");
+        sb.append("ID=").append(super.getID());
+        sb.append(", name=").append(super.getName());
+        sb.append(", username=").append(super.getUsername());
+        sb.append(", password=").append(super.getPassword());
+        sb.append(", email=").append(super.getEmail());
+        sb.append(", gender=").append(gender);
+        sb.append(", loginDate=").append(loginDate);
+        sb.append(", phoneNo=").append(phoneNo);
+        sb.append(", NRIC=").append(NRIC);
+        sb.append(", nationality=").append(nationality);
+        sb.append(", race=").append(race);
+        sb.append(", permanentAddress=").append(permanentAddress);
+        sb.append(", medicalCondition=").append(medicalCondition);
+        sb.append(", emerContactName=").append(emerContactName);
+        sb.append(", emerContactRelationship=").append(emerContactRelationship);
+        sb.append(", emerContactNo=").append(emerContactNo);
+        sb.append('}');
+        return sb.toString();
     }
     
     
--- src/main/java/hostelsystem/HostelSystem.java
+++ src/main/java/hostelsystem/HostelSystem.java
@@ -5,10 +5,17 @@ package hostelsystem;
  */
 
 import DesignUI.Splash;
+import Models.Student;
+import Utils.StudentHandling;
+import java.util.ArrayList;
 
 public class HostelSystem {
 
     public static void main(String[] args) {
+        ArrayList<Student> students = StudentHandling.getStudents();
+        for (Student student : students) {
+            System.out.println(student.toString());
+        }
         new Splash().start();
     }
 }
