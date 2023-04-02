aeac4ff1fcd10115ec51147b027dd926de41f0ac
QuasarX35 <shootingquasar555@gmail.com>
2023-04-02 15:20:26 +0800

    added roomtypes.txt

---
+++ src/main/java/Utils/RoomTypeHandling.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utils;
+
+/**
+ *
+ * @author KZ
+ */
+public final class RoomTypeHandling {
+    private static String PATH = "src/main/java/databases/room.txt";
+    
+}
