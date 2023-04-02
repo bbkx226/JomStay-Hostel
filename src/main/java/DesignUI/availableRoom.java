e327b37e2ca39d2042b4acd2cf21d4503d2c8b9c
QuasarX35 <shootingquasar555@gmail.com>
2023-04-02 23:08:26 +0800

    renamed AvailableRoom.java

--- src/main/java/DesignUI/availableRoom.java
+++ src/main/java/DesignUI/availableRoom.java
@@ -189,6 +189,22 @@ public class AvailableRoom extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
